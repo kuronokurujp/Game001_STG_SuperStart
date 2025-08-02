e8fb026d892a4266f5fcf16108cb0252723e388c
kuronokurujp <kuronokurujp@gmail.com>
2025-08-02 18:43:07 +0900

    [bugfix] vcpkgの変数の扱いにバグがあったので修正

--- HobbyEngine/cmake/VCPkg.cmake
+++ HobbyEngine/cmake/VCPkg.cmake
@@ -1,26 +1,26 @@
 # 初回設定
 function(vcpkg_configuration)
 
-	message("configuration vcpkg")
+	message("configuration vcpkg sourcr dir => ${CMAKE_SOURCE_DIR}")
 
 	# --- vcpkg ルートを決定 -------------------------------------------------
 
 	if (EXISTS "${CMAKE_SOURCE_DIR}/ThirdParty/vcpkg")
+		message("test")
 		# TODO:参照パス先が分かるので何か対策が必要かも
-		set(VCPKG_ROOT "${CMAKE_SOURCE_DIR}/ThirdParty/vcpkg" PARENT_SCOPE)
+		set(ENV{VCPKG_ROOT} "${CMAKE_SOURCE_DIR}/ThirdParty/vcpkg")
 	elseif(DEFINED ENV{VCPKG_ROOT} AND NOT "$ENV{VCPKG_ROOT}" STREQUAL "")
-		# 環境変数がセットされていればそれを優先
-		set(VCPKG_ROOT "$ENV{VCPKG_ROOT}" CACHE PATH "Path to vcpkg root" PARENT_SCOPE)
+		# 環境変数に設定されているのでそのまま利用
 	endif()
 
 	# 環境変数からvcpkgのパスを取得してツールチェインを設定
-	set(TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+	set(TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
 
 	# OS に応じて vcpkg 実行ファイル名を出し分け
 	if (WIN32)
-		set(VCPKG_COMMAND "${VCPKG_ROOT}/vcpkg.exe" PARENT_SCOPE)
+		set(ENV{VCPKG_COMMAND} "$ENV{VCPKG_ROOT}/vcpkg.exe")
 	else()
-		set(VCPKG_COMMAND "${VCPKG_ROOT}/vcpkg" PARENT_SCOPE)
+		set(ENV{VCPKG_COMMAND} "$ENV{VCPKG_ROOT}/vcpkg")
 	endif()
 endfunction()
 
@@ -28,27 +28,26 @@ endfunction()
 function(vcpkg_install_package install_dir vcpkg_triplet)
 
 	message("install vcpkg")
-	message(STATUS "Using vcpkg command: ${VCPKG_COMMAND}")
+	message(STATUS "Using vcpkg command: $ENV{VCPKG_COMMAND}")
 
 	# すでにvcpkgがインストール済みかどうかチェック
 	# statusファイルのパス
 	# vcpkg.jsonで記述したパッケージは全てインストールしているという前提でのチェック
 	# 特定のパッケージだけoptionalにした場合はチェック方法が変わるので注意
-	#set(STATUS_FILE "${install_dir}/ThirdParty/VcPkg/vcpkg/status")
+	set(STATUS_FILE "${install_dir}/ThirdParty/VcPkg/vcpkg/status")
 	execute_process(
 		COMMAND ${VCPKG_COMMAND} list --x-json
 		OUTPUT_VARIABLE _pkgs
 	)
 	string(JSON _dummy ERROR_VARIABLE _err LIST LENGTH _pkgs)
 	if(NOT _err STREQUAL "")
-		message(STATUS "vcpkgパッケージはすでにインストールされています。")
-		return()
+		# statusファイルの存在を確認
+		# .vspkg.jsonは存在するが、インストールしたvcpkgディレクトリがないケースもあるのでディレクトリチェックを加える
+		if(EXISTS "${STATUS_FILE}")
+			message(STATUS "vcpkgパッケージはすでにインストールされています。")
+			return()
+		endif()
 	endif()
-	# statusファイルの存在を確認
-	#if(EXISTS "${STATUS_FILE}")
-	#	message(STATUS "vcpkgパッケージはすでにインストールされています。")
-	#	return()
-	#endif()
 
 	# 一旦vcpkg関連のファイルを削除
 	# これはvcpkgを入れなおしたら環境再構築するため
@@ -66,7 +65,7 @@ function(vcpkg_install_package install_dir vcpkg_triplet)
 		if (VCPKG_FILE_SIZE GREATER 0)
 			# vcpkgの環境を構築
 			execute_process(
-				COMMAND cmd /c "${VCPKG_COMMAND}" "--vcpkg-root=${VCPKG_ROOT}" new --application
+				COMMAND cmd /c "$ENV{VCPKG_COMMAND}" "--vcpkg-root=$ENV{VCPKG_ROOT}" new --application
 				WORKING_DIRECTORY ${install_dir}
 				OUTPUT_VARIABLE CMD_OUTPUT
 				ERROR_VARIABLE CMD_ERROR
@@ -77,7 +76,7 @@ function(vcpkg_install_package install_dir vcpkg_triplet)
 			configure_file(${install_dir}/.vcpkg.json ${install_dir}/vcpkg.json COPYONLY)
 
 			execute_process(
-				COMMAND cmd /c "${VCPKG_COMMAND}" "--vcpkg-root=${VCPKG_ROOT}" install --clean-after-build "--x-manifest-root=${install_dir}" "--x-install-root=${install_dir}/ThirdParty/VcPkg" "--triplet=${vcpkg_triplet}"
+				COMMAND cmd /c "$ENV{VCPKG_COMMAND}" "--vcpkg-root=$ENV{VCPKG_ROOT}" install --clean-after-build "--x-manifest-root=${install_dir}" "--x-install-root=${install_dir}/ThirdParty/VcPkg" "--triplet=${vcpkg_triplet}"
 				WORKING_DIRECTORY ${install_dir}
 				OUTPUT_VARIABLE CMD_OUTPUT
 				ERROR_VARIABLE CMD_ERROR
